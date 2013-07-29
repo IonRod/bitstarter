2c3eaf333b7464465180049e570c98d3c64f3931
IonRod <rrod76@gmail.com>
2013-07-29 23:26:26 +0400

    Edited description in index.html

--- web.js
+++ web.js
@@ -6,11 +6,10 @@ var app = express.createServer(express.logger());
 var text = fs.readFileSync('index.html');
 var text = text.toString();
 
-console.log(text);
 app.get('/', function(request, response) {
-  
+response.send(text);  
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
 });
